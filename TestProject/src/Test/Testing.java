3c3655b564630111e58aa10e4bef2c40b2c77382
yn-automation <ynnsilva@gmail.com>
2021-02-20 23:32:44 +0530

    pckage Test

---
+++ TestProject/src/Test/Testing.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hello world Git & Jenkins");
+		
+	}
+
+}
